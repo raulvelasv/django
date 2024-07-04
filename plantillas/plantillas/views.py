50d3df75c6e5d6d08dcc8f7421e5da4543ea8378
Raul Velasquez <raul.velasquez@icaib.org>
2024-07-04 14:35:12 +0200

    comentarios y filtros

--- plantillas/plantillas/views.py
+++ plantillas/plantillas/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 def simple(request):
     return  render( request, 'simple.html',{})
 def dinamico(request,name):
-    categories = ['Programación', 'Fronted', 'Backend','DataBases']
+    categories = ['Programación', 'Frontend', 'Backend','DataBases']
     contexto = {'name':name, 'categories':categories}
     return render(request, 'dinamico.html', contexto)
